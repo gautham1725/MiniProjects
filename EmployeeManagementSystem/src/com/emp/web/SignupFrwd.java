7266e41574ae9498805c5e1d902169158a0e7e99
gautham1725 <39696268+gautham1725@users.noreply.github.com>
2018-08-31 10:26:42 +0530

    Add files via upload
    Employee Management System with login and signup options , getting employee details and filtering them with their age .

---
+++ EmployeeManagementSystem/src/com/emp/web/SignupFrwd.java
@@ -0,0 +1,27 @@
+package com.emp.web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name="frwd", urlPatterns="/frwd")
+public class SignupFrwd extends HttpServlet {
+
+	
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/signup.html");
+		dispatcher.forward(req, resp);
+		
+		
+	}
+
+}
